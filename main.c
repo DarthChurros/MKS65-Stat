5d2fd9ed367b3df9937f2570c59492273e914edd
DarthChurros <tnarayan00@stuy.edu>
2019-11-15 15:00:33 -0500

    permissions written

--- main.c
+++ main.c
@@ -5,9 +5,26 @@
 #include <sys/stat.h>
 
 int main() {
+  printf("%u\n", 7<<1);
   struct stat mf_data;
   stat("makefile", &mf_data);
+  printf("File size: %lldB\n", mf_data.st_size);
 
-  printf("File size: %lld\nPermissions: %o\nLast accessed: %ld\n", mf_data.st_size, mf_data.st_mode, mf_data.st_atime);
+  printf("Permissions: ");
+  char full_perms[] = "rwxrwxrwx";
+  char perms[10];
+  int i;
+  int j = 1;
+  for (i = 0; i < 9; i++) {
+    perms[sizeof(mode_t) * 4 - i] = '-';
+    if (j & mf_data.st_mode) {
+      perms[sizeof(mode_t) * 4 - i] = full_perms[sizeof(mode_t) * 4 - i];
+    }
+    j = j << 1;
+  }
+
+  printf("%s\n", perms);
+
+  printf("Last accessed: %ld\n", mf_data.st_atime);
   return 0;
 }
