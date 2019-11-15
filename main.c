26b7ff0d434d486e4c03d2ff92b9f87d0151b2e7
DarthChurros <tnarayan00@stuy.edu>
2019-11-15 11:21:26 -0500

    prints relevant but unreadable data

--- main.c
+++ main.c
@@ -5,6 +5,9 @@
 #include <sys/stat.h>
 
 int main() {
+  struct stat mf_data;
+  stat("makefile", &mf_data);
 
+  printf("File size: %lld\nPermissions: %o\nLast accessed: %ld\n", mf_data.st_size, mf_data.st_mode, mf_data.st_atime);
   return 0;
 }
